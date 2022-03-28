d806ebd6b86eb5623ff1f1c158b4530cae10c7c8
briandenicola <brian@briandenicola.com>
2022-03-28 10:54:04 -0500

    Fixed issue with kubelogin script

--- kubelogin.sh
+++ kubelogin.sh
@@ -14,12 +14,11 @@ echo -e "\e[34m»»» 📦 \e[32mInstalling \e[33m$NAME \e[35mv$VERSION\e[0m ...
 
 curl -sSL "https://github.com/azure/kubelogin/releases/download/v${VERSION}/kubelogin-linux-amd64.zip" -o /tmp/kubelogin.zip
 
-unzip /tmp/kubelogin.zip
+unzip /tmp/kubelogin.zip -d /tmp
 mkdir -p $INSTALL_DIR
-mv /tmp/kubelogin.zip $INSTALL_DIR
 mv /tmp/bin/linux_amd64/kubelogin $INSTALL_DIR
 rm -rf /tmp/kubelogin.zip
 rm -rf /tmp/bin
 
 echo -e "\n\e[34m»»» 💾 \e[32mInstalled to: \e[33m$(which $CMD)"
-echo -e "\e[34m»»» 💡 \e[32mVersion details: \n\e[39m$($CMD version)"
+echo -e "\e[34m»»» 💡 \e[32mVersion details: \n\e[39m$($CMD --version)"
