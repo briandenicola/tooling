5e6f1a25e6a31a672f5a279152bb1ec4f1bed169
briandenicola <brian@briandenicola.com>
2022-10-31 08:19:53 -0500

    Skaffold install

---
+++ skaffold.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+set -e
+
+VERSION=${1:-"0.0.0"}
+INSTALL_DIR=${2:-"$HOME/.local/bin"}
+CMD=skaffold
+NAME=skaffold
+
+echo -e "\e[34m»»» 📦 \e[32mInstalling \e[33m$NAME v$VERSION\e[0m ..."
+
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+sudo install skaffold /usr/local/bin/
+rm -rf skaffold
+
+echo -e "\n\e[34m»»» 💾 \e[32mInstalled to: \e[33m$(which $CMD)"
+echo -e "\e[34m»»» 💡 \e[32mVersion details: \e[39m$($CMD version)"
