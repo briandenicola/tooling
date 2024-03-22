a2c4c03dd150965e22e3951400e175e9a990cb9c
briandenicola <brian@briandenicola.com>
2024-03-22 09:07:27 -0500

    Updated template

--- _template.sh
+++ _template.sh
@@ -1,14 +1,21 @@
 #!/bin/bash 
 set -e
 
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" |
+  grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/'
+}
+
 VERSION=${1:-"0.0.0"}
 INSTALL_DIR=${2:-"$HOME/.local/bin"}
+
+REPO=__changeme__
 CMD=__changeme__
-NAME=__Change_Me__
+NAME=__changeme__
 
 echo -e "\e[34m»»» 📦 \e[32mInstalling \e[33m$NAME v$VERSION\e[0m ..."
 
 # INSTALL STEPS HERE
 
 echo -e "\n\e[34m»»» 💾 \e[32mInstalled to: \e[33m$(which $CMD)"
-echo -e "\e[34m»»» 💡 \e[32mVersion details: \e[39m$($CMD --version)"
+echo -e "\e[34m»»» 💡 \e[32mVersion details: \e[39m$($CMD version)"
