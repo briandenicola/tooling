76adf01bd3baf38cb3068f456808766e012ce473
briandenicola <brian@briandenicola.com>
2024-12-06 11:11:07 -0600

    🏒 Fri Dec  6 11:11:07 CST 2024

--- node.sh
+++ node.sh
@@ -6,9 +6,17 @@ NAME="Node.js"
 
 echo -e "\e[34m»»» 📦 \e[32mInstalling \e[33m$NAME \e[35mv$VERSION\e[0m ..."
 
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo npm install -g npm@latest
+#curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+#sudo apt install -y nodejs
+#sudo npm install -g npm@latest
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 22
 
 echo -e "\n\e[34m»»» 💾 \e[32mInstalled to: \e[33m$(which $CMD)"
 echo -e "\e[34m»»» 💡 \e[32mNode version details: \e[39m$($CMD --version)"
